731874e1a5b4aa2990f081d45122f03176f180b3
CheAle14 <thedark1513@gmail.com>
2020-02-16 08:52:30 +0000

    Installer mostly complete

---
+++ installer/ChessInstaller/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ChessInstaller
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbRegisterProtocol = new System.Windows.Forms.CheckBox();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.pbBar = new System.Windows.Forms.ProgressBar();
+            this.lblUpdate = new System.Windows.Forms.Label();
+            this.lblFolderFeedback = new System.Windows.Forms.Label();
+            this.lblBytes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(535, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chess Client Installation";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Folder:";
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(70, 62);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.ReadOnly = true;
+            this.txtLocation.Size = new System.Drawing.Size(363, 22);
+            this.txtLocation.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(439, 62);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(84, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(511, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Other Options";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cbRegisterProtocol
+            // 
+            this.cbRegisterProtocol.AutoSize = true;
+            this.cbRegisterProtocol.Checked = true;
+            this.cbRegisterProtocol.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbRegisterProtocol.Enabled = false;
+            this.cbRegisterProtocol.Location = new System.Drawing.Point(15, 154);
+            this.cbRegisterProtocol.Name = "cbRegisterProtocol";
+            this.cbRegisterProtocol.Size = new System.Drawing.Size(454, 21);
+            this.cbRegisterProtocol.TabIndex = 5;
+            this.cbRegisterProtocol.Text = "Register protocol, allowing starting of program directly from the web";
+            this.cbRegisterProtocol.UseVisualStyleBackColor = true;
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Enabled = false;
+            this.btnInstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstall.Location = new System.Drawing.Point(12, 239);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(511, 66);
+            this.btnInstall.TabIndex = 6;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // pbBar
+            // 
+            this.pbBar.Location = new System.Drawing.Point(12, 334);
+            this.pbBar.Name = "pbBar";
+            this.pbBar.Size = new System.Drawing.Size(511, 23);
+            this.pbBar.TabIndex = 7;
+            // 
+            // lblUpdate
+            // 
+            this.lblUpdate.Location = new System.Drawing.Point(12, 308);
+            this.lblUpdate.Name = "lblUpdate";
+            this.lblUpdate.Size = new System.Drawing.Size(421, 23);
+            this.lblUpdate.TabIndex = 8;
+            // 
+            // lblFolderFeedback
+            // 
+            this.lblFolderFeedback.AutoSize = true;
+            this.lblFolderFeedback.Location = new System.Drawing.Point(70, 91);
+            this.lblFolderFeedback.Name = "lblFolderFeedback";
+            this.lblFolderFeedback.Size = new System.Drawing.Size(266, 17);
+            this.lblFolderFeedback.TabIndex = 9;
+            this.lblFolderFeedback.Text = "Please select an empty folder to install to";
+            // 
+            // lblBytes
+            // 
+            this.lblBytes.Location = new System.Drawing.Point(463, 308);
+            this.lblBytes.Name = "lblBytes";
+            this.lblBytes.Size = new System.Drawing.Size(60, 23);
+            this.lblBytes.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 363);
+            this.Controls.Add(this.lblBytes);
+            this.Controls.Add(this.lblFolderFeedback);
+            this.Controls.Add(this.lblUpdate);
+            this.Controls.Add(this.pbBar);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.cbRegisterProtocol);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Installation";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox cbRegisterProtocol;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.ProgressBar pbBar;
+        private System.Windows.Forms.Label lblUpdate;
+        private System.Windows.Forms.Label lblFolderFeedback;
+        private System.Windows.Forms.Label lblBytes;
+    }
+}
+
---
+++ installer/ChessInstaller/MainForm.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessInstaller
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        bool isValidLocation(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return false;
+            if(Directory.Exists(path))
+            {
+                var cont = Directory.GetFiles(path);
+                if (cont.Length > 0)
+                    return false;
+                var fold = Directory.GetDirectories(path);
+                if (fold.Length > 0)
+                    return false;
+            } else
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        void continueRegistry()
+        {
+            if(this.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    continueRegistry();
+                }));
+                return;
+            }
+            setUpdate("Registering web protocol");
+            var key = Registry.ClassesRoot.CreateSubKey("chess", true);
+            key.SetValue("", "URL:chess Protocol");
+            key.SetValue("URL Protocol", "");
+            var keyShell = key.CreateSubKey("shell");
+            var keyOpen = keyShell.CreateSubKey("open");
+            var keyCom = keyOpen.CreateSubKey("command");
+            keyCom.SetValue("", $"\"{localPath}\" \"%1\" \"%2\" \"%3\" \"%4\" \"%5\" \"%6\" \"%7\" \"%8\" \"%9\"");
+            setUpdate("Creating empty registry");
+            var empty = Registry.CurrentUser.CreateSubKey("CheAle14");
+            var chess = empty.CreateSubKey("ChessClient");
+            chess.SetValue("", localPath);
+            chess.SetValue("InstalledOn", DateTime.Now.ToString());
+            setUpdate("Complete!");
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            lblFolderFeedback.Text = originalF;
+            lblFolderFeedback.ForeColor = Color.FromKnownColor(KnownColor.ControlText);
+            var browser = new FolderBrowserDialog();
+            browser.Description = "Select install folder";
+            browser.ShowNewFolderButton = true;
+            browser.SelectedPath = @"C:\Program Files (x86)\";
+            var r = browser.ShowDialog();
+            if(r == DialogResult.OK)
+            {
+                if(isValidLocation(browser.SelectedPath))
+                {
+                    txtLocation.Text = browser.SelectedPath;
+                } else
+                {
+                    lblFolderFeedback.Text = "Folder path is not valid (must be empty)";
+                    lblFolderFeedback.ForeColor = Color.Red;
+                }
+            } else
+            {
+                txtLocation.Text = "";
+            }
+            btnInstall.Enabled = !string.IsNullOrWhiteSpace(txtLocation.Text);
+        }
+
+        string originalF;
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if(args.Count() > 1)
+            {
+                MessageBox.Show(string.Join("\n", args), "Command Line");
+            }
+            var chess = Registry.CurrentUser.CreateSubKey("CheAle14").CreateSubKey("ChessClient");
+            var installPath = (string)chess.GetValue("");
+            if(!string.IsNullOrWhiteSpace(installPath))
+            {
+                localPath = installPath.Replace(Path.GetFileName(installPath), "");
+                txtLocation.Text = localPath;
+                btnBrowse.Enabled = false;
+                btnInstall.Text = "Uninstall";
+                btnInstall.Enabled = true;
+                lblFolderFeedback.Text = "Location of previous install found";
+            }
+
+            originalF = lblFolderFeedback.Text;
+            pbBar.Maximum = 100;
+        }
+
+        void setUpdate(string text)
+        {
+            if(this.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    setUpdate(text);
+                }));
+                return;
+            }
+            lblUpdate.Text = text;
+        }
+
+        void setPercentage(int perc, string additional)
+        {
+            if(this.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    setPercentage(perc, additional);
+                }));
+                return;
+            }
+            pbBar.Value = perc;
+            lblBytes.Text = additional;
+        }
+
+        public WebClient Downloader;
+        string url = "https://github.com/CheAle14/bot-chess/releases/download/v0.1/ChessInstaller.exe";
+        string localPath = "";
+        long lastKnown;
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            if(btnInstall.Text.StartsWith("Un"))
+            {
+                btnInstall.Enabled = false;
+                uninstall();
+            } else
+            {
+                if (!isValidLocation(txtLocation.Text))
+                    return;
+                btnInstall.Enabled = false;
+                install();
+            }
+        }
+
+        void uninstall()
+        {
+            try
+            {
+                Directory.Delete(localPath, true);
+            }
+            catch { }
+            Registry.ClassesRoot.DeleteSubKeyTree("chess");
+            var che = Registry.CurrentUser.CreateSubKey("CheAle14");
+            che.DeleteSubKey("ChessClient");
+            setUpdate("Program uninstalled");
+        }
+
+        void install()
+        {
+            Downloader = new WebClient();
+            Downloader.DownloadProgressChanged += Downloader_DownloadProgressChanged;
+            Downloader.DownloadFileCompleted += Downloader_DownloadFileCompleted;
+            localPath = Path.Combine(txtLocation.Text, "ChessInstaller.exe");
+            Downloader.DownloadFileAsync(new Uri(url),
+                localPath);
+        }
+
+        private void Downloader_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if(e.Cancelled || e.Error != null)
+            {
+                setUpdate("Failed: " + (e.Error?.Message ?? "Cancelled"));
+            } else
+            {
+                setPercentage(100, formatBytes(lastKnown));
+                setUpdate("Download complee");
+                continueRegistry();
+            }
+        }
+
+        string formatBytes(long bytes)
+        {
+            long gb = bytes / (1024 * 1024 * 1024);
+            if (gb > 0)
+                return $"{gb}GB";
+            long mb = bytes / (1024 * 1024);
+            if (mb > 0)
+                return $"{mb}MB";
+            long kb = bytes / (1024);
+            if (kb > 0)
+                return $"{kb}KB";
+            return $"{bytes}B";
+        }
+
+        private void Downloader_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            lastKnown = e.BytesReceived;
+            setUpdate($"Downloading {url}");
+            setPercentage(e.ProgressPercentage, formatBytes(e.BytesReceived));
+        }
+    }
+}
